a1705f34953f95b2f2f03d163aedde4ea0fd2e1d
Stone.Yu <Stone.Yu@meituns-MacBook-Pro.local>
2016-08-26 14:53:33 +0800

    add podfile

---
+++ PodspecTest/Pods/Target Support Files/Pods-PodspecTest/Pods-PodspecTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodspecTest : NSObject
+@end
+@implementation PodsDummy_Pods_PodspecTest
+@end
---
+++ PodspecTest/PodspecTest/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  PodspecTest
+//
+//  Created by Stone.Yu on 16/8/26.
+//  Copyright © 2016年 Stone.Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
